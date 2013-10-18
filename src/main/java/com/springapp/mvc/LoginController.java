537df24b25eb3d97c6120ce04ce832b1ca2a88c6
miyamoto_s <dev.miyamoto.s@gmail.com>
2013-10-18 21:46:55 +0900

    Add:Pageディレクトリ

--- src/main/java/com/springapp/mvc/LoginController.java
+++ src/main/java/com/springapp/mvc/LoginController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/Login")
+@RequestMapping("/login")
 public class LoginController {
     @RequestMapping(method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
+    public String printWelcome() {
 //        model.addAttribute("message", "Hello world!");
         return "login";
     }
